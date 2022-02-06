8aef8833af11ae76b5112b4a124626f5893b2922
Derek Zhang <derek.zhang0210@gmail.com>
2022-02-06 16:03:52 -0500

    add/edit scripts

--- char_searcher.py
+++ char_searcher.py
@@ -3,29 +3,47 @@ import docx;
 
 s = [];
 
-direc = str(input("Directory: "))
-n= int(input("# of char:"))
+direcs = ['Act 3 Prim']#['Act 1', 'Act 2 Lilith', 'Act 2 Prim', 'Act 3 Lilith','Act 3 Prim']
+
+n= int(input("# of char: "))
+
+strict = False
+if n > 1:
+    inp = input("Strict? (y/n): ")
+    if inp.strip() == 'y':
+        strict = True
+
 searches = []
 for i in range(n):
     searches.append(str(input("Character: ")))
 
 total = 0;
 
-for f in os.listdir("./"+direc):
-    doc = docx.Document("./"+direc+"/"+f)
-    found = []
-    for line in doc.paragraphs:
-        for run in line.runs:
-            for search in searches:
-                if search in found:
-                    continue
-                if search.lower() in run.text.lower().split() or search.lower()+'.' in run.text.lower().split():
-                    found.append(search)
-                if len(found) == len(searches):
-                    s.append(f)
+for direc in direcs:
+    print(direc)
+    for f in os.listdir("./"+direc):
+        doc = docx.Document("./"+direc+"/"+f)
+        found = []
+        done = False
+        for line in doc.paragraphs:
+            for run in line.runs:
+                for search in searches:
+                    if search in found:
+                        continue
+                    text = run.text.lower().split()
+                    if search.lower() in text or search.lower()+'.' in text or search.lower()+":" in text or "?"+search.lower() in text:
+                        found.append(search)
+                        if not strict:
+                            print("    "+f)
+                            done = True
+                            break
+                    if len(found) == len(searches):
+                        print("    "+f)
+                        break
+                if done:
                     break
-        if len(found) == len(searches):
-            break
+            if done or len(found) == len(searches):
+                break
 
 for f in s:
     print(f)
--- expression_searcher2.py
+++ expression_searcher2.py
@@ -1,30 +1,49 @@
 import os;
 import docx; 
 
-character = str(input("Character: "))
 total = 0;
 
 direcs = ['Act 1', 'Act 2 Lilith', 'Act 2 Prim', 'Act 3 Lilith','Act 3 Prim']
 
+keywords = ['Asher', '?Asher', 'Asher:', '?Asher:']
+search = 'neutral concern'
+
+store = {}
+
 for direc in direcs:
+    print(direc)
     for f in os.listdir("./"+direc):
         doc = docx.Document("./"+direc+"/"+f)
         expressions = {}
+        done = False
         for line in doc.paragraphs:
             for run in line.runs:
                 s = run.text.split()
-                if len(s)>=3 and s[0] == character:
+                if len(s)>=3 and s[0] in keywords:
                     if '(' in s[1] and ')' in s[2]:
-                        key = s[1][1:]+" "+s[2][:-2]
-                        if key in expressions:
-                            expressions[key]+=1
-                        else:
-                            expressions[key]=1
-        search = ['on phone']
-        tf = True
-        for expr in search:
-            if expr not in expressions:
-                tf = False
-        if tf:
-            print(direc + ' ' +f)
+                        b1 = s[1].index('(')
+                        b2 = s[2].index(')')
+                        expr = s[1][b1+1:] + " " + s[2][:b2]
+                        if s[1][b1+1:] == 'excited' or s[1][b1+1:] == 'eating':
+                            print("    "+f)
+                            done = True
+                            break
+                            '''
+                            if f"{s[0]} {expr}" in store:
+                                store[f"{s[0]} {expr}"].add(f'{direc}/{f}')
+                            else:
+                                store[f"{s[0]} {expr}"] = set([f'{direc}/{f}'])
+                            '''
+            if done:
+                break
+    print()
 
+
+'''
+for k in store:
+    print(k)
+    for x in store[k]:
+        print("    "+x)
+
+    print()
+'''
