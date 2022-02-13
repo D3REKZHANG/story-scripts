25fbdf5e58a5e878f1157a24683c8684fb0121bf
Derek Zhang <derek.zhang0210@gmail.com>
2022-02-12 21:08:11 -0500

    updates!

--- background_verify.py
+++ background_verify.py
@@ -15,49 +15,21 @@ for direc in direcs:
     for f in os.listdir(f'./{direc}'):
         doc = docx.Document("./"+direc+"/"+f)
         for line in doc.paragraphs:
-            for run in line.runs:
-                if run.bold:
-                    if 'OR' in run.text:
-                        continue
-                    cleaned = run.text.replace("’", "'").replace("–", "-")
+            if line.runs[0].bold:
+                if 'OR' in line.text:
+                    continue
+                cleaned = line.text.replace("’", "'").replace("–", "-")
 
-                    if 'Cutscene' in cleaned or 'Nostalgia' in cleaned:
-                        if cleaned in cutscenes:
-                            cutscenes[cleaned].add(f'{direc}/{f}')
-                        else:
-                            cutscenes[cleaned] = set([f'{direc}/{f}'])
-                        continue
+                if 'Cutscene' in cleaned or 'Nostalgia' in cleaned or 'End Scene' in cleaned:
+                    continue
 
-                    if cleaned in backgrounds:
-                        backgrounds[cleaned].add(f'{direc}/{f}')
-                    elif cleaned in non_bg:
+                if cleaned not in backgrounds:
+                    if cleaned in non_bg:
                         non_bg[cleaned].add(f'{direc}/{f}')
                     else:
                         non_bg[cleaned] = set([f'{direc}/{f}'])
 
-empty = []
-for bg in backgrounds:
-    if len(backgrounds[bg]) == 0:
-        empty.append(bg)
-        continue
-    print(bg)
-    for f in sorted(backgrounds[bg]):
-        print("    "+f)
-    print()
-
-print("Backgrounds not used:")
-for e in empty:
-    print(e)
-    
-print("\nCUTSCENES ----------\n")
-
-for bg in cutscenes:
-    print(bg)
-    for f in cutscenes[bg]:
-        print("    "+f)
-    print()
-
-print("\nOTHER ----------\n")
+print("\nERRORS? ----------\n")
 
 for bg in non_bg:
     print(bg)
