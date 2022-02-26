08ab274f1700e6ce24292a21224edda7ce73907d
Derek Zhang <derek.zhang0210@gmail.com>
2022-02-26 16:44:55 -0500

    updates

--- char_searcher2.py
+++ char_searcher2.py
@@ -1,25 +1,37 @@
 import os;
 import docx; 
 
-s = [];
 
-chars = ['Mara', 'Lilith', 'Prim', 'Petra', 'Mom', 'Asher', 'Teacher', 'Mitch', 'Iris', 'Kara', 'Petrov', 'Greta' ]
-direcs = ['Act 3 Prim']#['Act 1', 'Act 2 Lilith', 'Act 2 Prim', 'Act 3 Lilith','Act 3 Prim']
+chars = ['Mara', 'Lilith', 'Prim', 'Petra', 'Mom', 'Asher', 'Teacher', 'Mick', 'Iris', 'Aunt', 'Petrov', 'Greta' ]
+direcs = ['Act 3 Lilith']#['Act 1', 'Act 2 Lilith', 'Act 2 Prim', 'Act 3 Lilith','Act 3 Prim']
+
+s = {char:[] for char in chars}
 
 for char in chars:
-    print(char)
     for direc in direcs:
-        print(direc)
         for f in os.listdir("./"+direc):
             doc = docx.Document("./"+direc+"/"+f)
             done = False
             for line in doc.paragraphs:
                 for run in line.runs:
-                    text = run.text.lower().split()
-                    if char.lower() in text or char.lower()+'.' in text or char.lower()+":" in text or "?"+char.lower() in text:
-                        print("    "+f)
+                    text = run.text.lower()
+                    if char.lower()+":" in text:
+                        s[char].append(f)
                         done = True
                         break
                 if done:
                     break
-    print("---------\n")
+
+def getNum(s):
+    n = ""
+    for c in s:
+        if c.isdigit():
+            n+=c
+    return int(n)
+
+for char in s:
+    print(char)
+    l = sorted(s[char], key=lambda x: getNum(x))
+    for f in l:
+        print(f"    {f}")
+    print()
